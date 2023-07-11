aec45dca081cec881e88a5b4a8e411e1abceb0c7
yakupsuda <yakupsuda@gmail.com>
2023-07-11 11:08:56 +0300

    Initial Commit

---
+++ JokesApp(SwiftUI)/JokesApp_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp_SwiftUI_App.swift
+//  JokesApp(SwiftUI)
+//
+//  Created by Yakup Suda on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
